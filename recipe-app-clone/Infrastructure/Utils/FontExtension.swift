e9da721efd6e5f249a7876e773e8e5f790036146
Moon <1tjsen@naver.com>
2025-07-22 19:08:42 +0900

    feat: FontExtension 추가

---
+++ recipe-app-clone/Infrastructure/Utils/FontExtension.swift
@@ -0,0 +1,28 @@
+//
+//  FontExtension.swift
+//  recipe-app-clone
+//
+//  Created by 조호준 on 7/22/25.
+//
+
+import SwiftUI
+
+extension Font {
+    // Bold
+    static let titleTextBold = Font.custom("Poppins-Bold", size: 50)
+    static let headerTextBold = Font.custom("Poppins-Bold", size: 30)
+    static let largeTextBold = Font.custom("Poppins-Bold", size: 20)
+    static let mediumTextBold = Font.custom("Poppins-Bold", size: 18)
+    static let normalTextBold = Font.custom("Poppins-Bold", size: 16)
+    static let smallTextBold = Font.custom("Poppins-Bold", size: 14)
+    static let smallerTextBold = Font.custom("Poppins-Bold", size: 11)
+    
+    // Regular
+    static let titleTextRegular = Font.custom("Poppins-Regular", size: 50)
+    static let headerTextRegular = Font.custom("Poppins-Regular", size: 30)
+    static let largeTextRegular = Font.custom("Poppins-Regular", size: 20)
+    static let mediumTextRegular = Font.custom("Poppins-Regular", size: 18)
+    static let normalTextRegular = Font.custom("Poppins-Regular", size: 16)
+    static let smallTextRegular = Font.custom("Poppins-Regular", size: 14)
+    static let smallerTextRegular = Font.custom("Poppins-Regular", size: 11)
+}
